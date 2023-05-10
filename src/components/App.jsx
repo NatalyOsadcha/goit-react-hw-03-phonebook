7a756cb89778f407f49d5bd9844f5bca1f04b300
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-05-10 22:48:19 +0300

    added part from ht-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import PhonebookForm from './PhonebookForm/PhonebookForm';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+import initialContacts from '../InitialContacts.json';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: initialContacts,
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    if (this.state.contacts.some(contact => contact.name === name)) {
+      alert(`${name} is already in the contacts.`);
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  changeFilter = evt => {
+    this.setState({
+      filter: evt.target.value,
+    });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <PhonebookForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <Contacts
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
